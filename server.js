042bf096db8b7d8724d8d3cf1b7dee3c88ec45a9
VictorGreco <victorgrecotech@gmail.com>
2021-09-15 01:50:27 +0200

    fix: functionality

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const validUrl = require('valid-url');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const shorurlSchema = new Schema({
@@ -23,9 +22,6 @@ const shorurlSchema = new Schema({
 
 const Shorturl = mongoose.model('Shorturl', shorurlSchema);
 
-const ERROR_404_MESSAGE = 'Not found';
-const PARSED_URLS_STORAGE = [null];
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -33,17 +29,6 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-
-const loggerMiddleware = (req, res, next) => {
-  const method = req.method;
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const path = req.originalUrl;
-  const value = req.body.url || req.params.short_url;
-
-  console.log(`${method} - ${ip} / ${path} - ${value}`);
-  next();
-}
 
 const validateUrlMiddleware = (req, res, next) => {
   if (!validUrl.isWebUri(req.body.url)) {
@@ -70,6 +55,11 @@ const storeUniqueUrlMiddleware = async (req, res, next) => {
 
 const validateStoredShorturl = async (req, res, next) => {
   const { short_url } = req.params;
+
+  if (isNaN(+short_url)) {
+    res.json({ error: 'invalid url' });
+  }
+
   const shorturlItem = await Shorturl.find({ short_url });
 
   if (shorturlItem.length === 0) {
@@ -86,16 +76,20 @@ const postShorturlHandler = async (req, res) => {
 
   const { original_url, short_url } = shorturlItem[0];
 
-  res.json({  });
+  res.json({  original_url, short_url });
 }
 
 const getWildCardHandler = (req, res) => { 
-  res.status(404).send(ERROR_404_MESSAGE);
+  res.status(404).send('Not found');
 }
 
 const getShorturlByCodeHandler = async(req, res) => {
   const { short_url } = req.params
   const shorturlItem = await Shorturl.find({ short_url });
+
+  if (shorturlItem.length === 0) {
+    res.json({ error: 'invalid url' })
+  }
 
   res.redirect(shorturlItem[0].original_url);
 }
